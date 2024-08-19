87461295e5de88a23937815de8c51765e3a3122e
Ronin Zheng <1752551862@qq.com>
2024-08-19 14:44:11 +0800

    Update index.tsx

--- src/modules/index.tsx
+++ src/modules/index.tsx
@@ -1,2 +1,13 @@
+import { LatLng } from "leaflet";
+
 export { default as List } from "./List";
 export { default as Map } from "./Map";
+
+export type DataType = {
+    coors: [LatLng[]]
+    name: string
+    borderColor: string
+    fillColor: string
+    visible: boolean
+    id: number
+}
